289344d2c0414cea34ac97782035c3987b4c40f4
Manuel Guevara <mguevara@webcreek.com>
2022-01-28 10:02:53 -0400

    setting up

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,11 @@ import ReactDOM from "react-dom";
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
+//include bootstrap npm library into the bundle
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.esm.js";
+import bootstrap from "bootstrap";
+
 //import your own components
 import Layout from "./layout";
 
